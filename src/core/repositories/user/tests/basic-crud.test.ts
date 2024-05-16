285bc97870d9968be4afedc6293eb1b3c24549ec
Šimon Zajíček <simon.zajicek@qest.cz>
2024-05-16 19:55:56 +0200

    vitest works

--- src/core/repositories/user/tests/basic-crud.test.ts
+++ src/core/repositories/user/tests/basic-crud.test.ts
@@ -1,12 +1,16 @@
 import { describe, beforeAll, it, expect } from 'vitest'
+import { PrismaClient } from '@prisma/client'
 import { IUserRepository } from '..'
 import { User } from '@root/core/entities/user'
 import { HasMany } from '@root/core/entities/helpers/relationship'
+import { PrismaUserRepository } from '@root/infrastructure/repositories/user/prisma'
 
 describe('UserRepository basic CRUD', () => {
     let userRepository: IUserRepository
 
     beforeAll(async () => {
+        const prisma = new PrismaClient()
+        userRepository = new PrismaUserRepository(prisma)
         // some init
         // userRepository = new UserRepository()
     })
@@ -14,14 +18,14 @@ describe('UserRepository basic CRUD', () => {
     it('should insert one user and retrieve it', async () => {
         const userToCreate = User.create({
             articles: HasMany.unloaded('user.articles'),
-            email: '',
-            password: '',
-            username: '',
+            email: 'email@mail',
+            password: 'as',
+            username: 'asga',
         })
 
         const inserted = await userRepository.insertOne(userToCreate.data)
 
         const found = await userRepository.findById(inserted.id)
-        expect(found).toEqual(inserted)
+        expect(found).toMatchObject(inserted)
     })
 })
